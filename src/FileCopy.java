842057543071365e5e98f1bb3ba6904d43f416c3
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 15:47:45 +0530

    Update FileCopy.java

--- src/FileCopy.java
+++ src/FileCopy.java
@@ -2,33 +2,39 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class FileCopyByteStreams {
+public class FileCopy {
     public static void main(String[] args) {
         String sourceFile = "source.txt";
         String destFile = "destination.txt";
 
+        // Part 1: Copy from source to destination using Byte Streams
         try (FileInputStream fis = new FileInputStream(sourceFile);
              FileOutputStream fos = new FileOutputStream(destFile)) {
 
             int byteData;
+
             while ((byteData = fis.read()) != -1) {
                 fos.write(byteData);
             }
-            
-            System.out.println("File Copied");
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error during file copy: " + e.getMessage());
+            return;
         }
 
-        try (FileInputStream fisDest = new FileInputStream(destFile)) {
+        System.out.println("File Copied");
+
+        // Part 2: Read and display the contents of destination file
+        try (FileInputStream fis = new FileInputStream(destFile)) {
+
             int byteData;
-            while ((byteData = fisDest.read()) != -1) {
+
+            while ((byteData = fis.read()) != -1) {
                 System.out.print((char) byteData);
             }
-            System.out.println();
+
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error reading destination file: " + e.getMessage());
         }
     }
 }
